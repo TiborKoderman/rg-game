414ed33994802e0971d3c1b3d70b4e0f796578c4
tiborkoderman <tibor.koderman@gmail.com>
2022-12-30 19:24:29 +0100

    basically kopija

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Application } from "../common/Application.js";
+import { Application } from "../common/engine/Application.js";
 
 import { Renderer } from "./Renderer.js";
 import { Physics } from "./Physics.js";
@@ -7,57 +7,61 @@ import { SceneLoader } from "./SceneLoader.js";
 import { SceneBuilder } from "./SceneBuilder.js";
 
 class App extends Application {
-  async start() {
-    const gl = this.gl;
 
-    this.renderer = new Renderer(gl);
+    async start() {
+        const gl = this.gl;
 
-    this.gl.canvas.addEventListener("click", (e) =>
-      this.gl.canvas.requestPointerLock()
-    );
-    document.addEventListener("pointerlockchange", (e) => {
-      if (document.pointerLockElement === this.gl.canvas) {
-        this.camera.enable();
-      } else {
-        this.camera.disable();
-      }
-    });
-  }
+        this.renderer = new Renderer(gl);
 
-  async load(uri) {
-    const builder = new SceneBuilder(scene);
-    this.scene = builder.build();
-    this.physics = new Physics(this.scene);
+        await this.load('./src/scene.json');
 
-    // Find first camera.
-    this.camera = null;
-    this.scene.traverse((node) => {
-      if (node instanceof Camera) {
-        this.camera = node;
-      }
-    });
+        this.gl.canvas.addEventListener('click', e => this.gl.canvas.requestPointerLock());
+        document.addEventListener('pointerlockchange', e => {
+            if (document.pointerLockElement === this.gl.canvas) {
+                this.camera.enable();
+            } else {
+                this.camera.disable();
+            }
+        });
+    }
 
-    this.camera.aspect = this.aspect;
-    this.camera.updateProjection();
-    this.renderer.prepare(this.scene);
-  }
+    async load(uri) {
+        const scene = await new SceneLoader().loadScene(uri);
+        const builder = new SceneBuilder(scene);
+        this.scene = builder.build();
+        this.physics = new Physics(this.scene);
 
-  update(time, dt) {
-    this.camera.update(dt);
-    this.physics.update(dt);
-  }
+        // Find first camera.
+        this.camera = null;
+        this.scene.traverse(node => {
+            if (node instanceof Camera) {
+                this.camera = node;
+            }
+        });
 
-  render() {
-    this.renderer.render(this.scene, this.camera);
-  }
+        this.camera.aspect = this.aspect;
+        this.camera.updateProjection();
+        this.renderer.prepare(this.scene);
+    }
 
-  resize(width, height) {
-    this.camera.aspect = width / height;
-    this.camera.updateProjection();
-  }
+    update(time, dt) {
+        this.camera.update(dt);
+        this.physics.update(dt);
+    }
+
+    render() {
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    resize(width, height) {
+        this.camera.aspect = width / height;
+        this.camera.updateProjection();
+    }
+
 }
 
-const canvas = document.querySelector("canvas");
+const canvas = document.querySelector('canvas');
 const app = new App(canvas);
 await app.init();
-document.querySelector(".loader-container").remove();
+document.querySelector('.loader-container').remove();
+
