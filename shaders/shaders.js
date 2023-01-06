bc382305f624411ea9131d7eafb80a62e9cfc498
tiborkoderman <tibor.koderman@gmail.com>
2023-01-06 02:48:31 +0100

    updated shaders

--- shaders/shaders.js
+++ shaders/shaders.js
@@ -3,6 +3,7 @@ layout (location = 0) in vec4 aPosition;
 layout (location = 1) in vec2 aTexCoord;
 uniform mat4 uViewModelMatrix;
 uniform mat4 uProjectionMatrix;
+
 out vec2 vTexCoord;
 void main() {
     vTexCoord = aTexCoord;
